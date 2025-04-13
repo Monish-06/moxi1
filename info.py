2d4b1b063b50872bc33126272e1f78440715502e
Monish-06 <70092044+Monish-06@users.noreply.github.com>
2025-04-13 13:00:59 +0530

    Update info.py

--- info.py
+++ info.py
@@ -126,7 +126,7 @@ VERIFY_SND_SHORTLINK_API = environ.get('VERIFY_SND_SHORTLINK_API', '')
 
 # Shortlink Info
 SHORTLINK_MODE = bool(environ.get('SHORTLINK_MODE', True)) # Set True Or False
-SHORTLINK_URL = environ.get('SHORTLINK_URL', 'https://gplinks.co')
+SHORTLINK_URL = environ.get('SHORTLINK_URL', 'gplinks.co')
 SHORTLINK_API = environ.get('SHORTLINK_API', 'f65177326ab770228557570f255893db54df50da')
 TUTORIAL = environ.get('TUTORIAL', '') # How Open Shortner Link Video Link , Channel Link Where You Upload Your Video.
 
